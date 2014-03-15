fd02eccb9302b574fa39717136b9843a7ff62117
Kenny Darrell <darrelkj@gmail.com>
2014-03-15 19:00:43 -0400

    add a bunch of functions for rough functionailty

--- r2048.R
+++ r2048.R
@@ -5,7 +5,6 @@ inits <- sample(1:16, 2)
 
 start[inits] <- 2
 
-
 board <- matrix(start, 4, 4)
 
 
@@ -13,16 +12,125 @@ board <- matrix(start, 4, 4)
 print_board <- function(b) {
   for(i in seq(4)) {
     for (j in seq(4)) {
-      if (board[i, j] == 0) {
+      if (b[i, j] == 0) {
         #cat('|   ')
-        cat(' . ')
+        cat('  .  ')
       } else {
-        cat('', board[i, j])
+        cat(' ', b[i, j], ' ')
         #cat(paste('| ', board[i, j], ' ', sep = ''))
       }
     }
-    #cat('|\n')
+    cat('\n')
   }
 }
 
-print_board(board)
+
+moves <- c('u', 'd', 'l', 'r')
+
+# Is the given move valid
+valid_move <- function(b, dir) {
+  dir == 'd' & any(apply(sign(b), 2, diff) == -1) |
+    dir == 'u' & any(apply(sign(b), 2, diff) == 1) |   
+    dir == 'l' & any(apply(sign(b), 1, diff) == 1) |
+    dir == 'r' & any(apply(sign(b), 1, diff) == -1)
+}
+
+
+ind3 <- function(x) {
+  x[1] == x[2] & x[1] != 0 |
+    x[2] == x[3] & x[2] != 0 |
+    x[3] == x[4] & x[3] != 0
+}
+
+
+valid_move2 <- function(b, dir) {
+  dir == 'd' & any(apply(b, 2, ind3)) |
+    dir == 'u' & any(apply(b, 2, ind3)) |   
+    dir == 'l' & any(apply(b, 1, ind3)) |
+    dir == 'r' & any(apply(b, 1, ind3))
+}
+
+# If there are no valid moves it is game over.
+game_over <- function(b) {
+  !any(valid_move(b, 'u'), valid_move(b, 'd'), 
+      valid_move(b, 'l'), valid_move(b, 'r'))
+}
+
+
+# Give extra zeros to make length 4.
+shift_left <- function(v) c(rep(0, 4 - length(v)), v)
+shift_right <- function(v) c(v, rep(0, 4 - length(v)))
+
+rotate <- function(x) t(apply(x, 2, rev))
+
+ind <- function(x) {
+  if (x[1] == x[2] & x[3] == x[4] & x[1] != 0 & x[3] != 0) {
+    c(2*x[1], 2*x[3], 0, 0)
+  } else if (x[1] == x[2] & x[1] != 0) {
+    c(2*x[1], x[3], x[4], 0)
+  } else if (x[2] == x[3] & x[2] != 0) {
+    c(x[1], 2*x[2], x[4], 0)
+  } else if (x[3] == x[4] & x[3] != 0) {
+    c(x[1], x[2], 2*x[4], 0)
+  } else x
+}
+
+
+ind2 <- function(x) {
+  if (x[1] == x[2] & x[3] == x[4] & x[1] != 0 & x[3] != 0) {
+    c(0, 0, 2*x[1], 2*x[3])
+  } else if (x[1] == x[2] & x[1] != 0) {
+    c(0, 2*x[1], x[3], x[4])
+  } else if (x[2] == x[3] & x[2] != 0) {
+    c(x[1], 0, 2*x[2], x[4])
+  } else if (x[3] == x[4] & x[3] != 0) {
+    c(x[1], x[2], 0, 2*x[4])
+  } else x
+}
+
+# pad(b[, 1][b[, 1] != 0]
+move <- function(b, dir) {
+  if(dir == 'd') apply(b, 2, function(x) shift_left(x[x != 0]))
+  else if (dir == 'u') apply(b, 2, function(x) shift_right(x[x != 0]))
+  else if (dir == 'r') rotate(apply(b, 1, function(x) rev(shift_left(x[x != 0]))))
+  else t(apply(b, 1, function(x) shift_right(x[x != 0])))
+}
+
+
+agg <- function(b, dir) {
+  if(valid_move(b, dir) | valid_move2(b, dir)) {
+    b <- move(b, dir)
+    b <- if(dir == 'd') apply(b, 2, ind)
+    else if (dir == 'u') apply(b, 2, ind2)
+    else if (dir == 'l') t(apply(b, 1, ind))
+    else t(apply(b, 1, ind2))
+    b <- move(b, dir)
+    b[sample(which(b == 0), 1)] <- 2
+    b
+  } else b
+}
+
+
+
+
+play <- function(b) {
+  print_board(board)
+  while(!game_over(b)) {
+    n <- scan(nmax = 1, quiet = T, what = character())
+    b <- agg(b, n)
+    print_board(b)
+  }
+  print('Game Over')
+}
+
+
+# move(board, 'd')
+# move(board, 'u')
+# move(board, 'l')
+# move(board, 'r')
+
+
+play(board)
+
+
+
